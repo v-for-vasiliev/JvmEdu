6a7f37a68d51f4e1889dd7a5e74bf6e7362d958d
Kirill Vasiliev <vasiliev.k.v@gmail.com>
2019-11-26 00:05:37 +0300

    JSON tests

--- src/main/java/ru/jvm/edu/json/Main.java
+++ src/main/java/ru/jvm/edu/json/Main.java
@@ -7,9 +7,20 @@ public class Main {
     private static Gson gson = new Gson();
 
     public static void main(String[] args) {
+        fromJsonToObject();
+        fromObjectToJson();
+    }
+
+    public static void fromJsonToObject() {
         String authMessageJson = "{ \"login\" : \"puhan\", \"password\": \"12345\" }";
         AuthMessage authMessageObject = gson.fromJson(authMessageJson, AuthMessage.class);
         System.out.println("AuthMessage object: login=" + authMessageObject.login + ", password="
             + authMessageObject.password);
+    }
+
+    public static void fromObjectToJson() {
+        AuthMessage authMessageObject = new AuthMessage("Zaika", "54321");
+        String authMessageJson = gson.toJson(authMessageObject);
+        System.out.println("AuthMessage json: " + authMessageJson);
     }
 }
